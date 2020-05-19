ee43d3709ebf35e09f034aef545f19badd6886c9
Just-Doing <969502899@qq.com>
2020-05-19 23:33:18 +0800

    包更新

--- packages/sharewinfo-utils/src/url.js
+++ packages/sharewinfo-utils/src/url.js
@@ -3,7 +3,7 @@ export function getPageQuery() {
   return parse(window.location.href.split("?")[1]);
 }
 
-// bool
+//path: url路径 bool
 export function isUrl(path) {
   const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
   return reg.test(path);
