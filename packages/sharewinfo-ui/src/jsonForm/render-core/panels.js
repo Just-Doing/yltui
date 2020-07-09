43a4bb9e6c64fdd7f415c24964e65ab10da78b86
yanglin <yanglin@sharewinfo.com>
2020-07-09 14:16:37 +0800

    panel  样式调整

--- packages/sharewinfo-ui/src/jsonForm/render-core/panels.js
+++ packages/sharewinfo-ui/src/jsonForm/render-core/panels.js
@@ -9,9 +9,7 @@ export default option => {
 
   const panelHeader = document.createElement('div');
   panelHeader.setAttribute('class', 'panel-header');
-  panelHeader.innerHTML = `<svg viewBox="64 64 896 896" focusable="false" data-icon="right" width="1em" height="1em" fill="currentColor" aria-hidden="true" style="transform: rotate(90deg)">
-      <path d="M765.7 486.8L314.9 134.7A7.97 7.97 0 00302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 000-50.4z"></path>
-      </svg>
+  panelHeader.innerHTML = `<svg viewBox="0 0 1024 1024" focusable="false" class="" data-icon="caret-right" width="1em" height="1em" fill="currentColor" aria-hidden="true" style=""><path d="M715.8 493.5L335 165.1c-14.2-12.2-35-1.2-35 18.5v656.8c0 19.7 20.8 30.7 35 18.5l380.8-328.4c10.9-9.4 10.9-27.6 0-37z"></path></svg>
       <span>${option.text}</span>
     `;
 
