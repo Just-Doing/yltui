0bf66b9c1c8ba49400597fe18f0ff232a9f9ab71
yanglin <yanglin@sharewinfo.com>
2020-07-14 10:10:13 +0800

    时间格式化

--- packages/sharewinfo-utils/src/time.js
+++ packages/sharewinfo-utils/src/time.js
@@ -45,10 +45,10 @@ export function getTimeDistance(type) {
 }
 
 // 转换时间 为 yyyy-MM-DD hh:mm:ss
-export const formate = (inputDate) => {
+export const formate = inputDate => {
   let time = '';
-  if (updateDate) {
-    const date = new Date(updateDate);
+  if (inputDate) {
+    const date = new Date(inputDate);
     const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
     const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
     const hours = date.getHours() > 9 ? date.getHours() : `0${date.getHours()}`;
