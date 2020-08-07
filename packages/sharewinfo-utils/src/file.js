35623a28b49b11b73d4fc462a45cd2f892dbccfa
yanglin <yanglin@sharewinfo.com>
2020-08-07 09:36:02 +0800

    包更新

--- packages/sharewinfo-utils/src/file.js
+++ packages/sharewinfo-utils/src/file.js
@@ -1,22 +1,17 @@
-const fetch = function() {};
+const fetch = function () {};
 
-export const UploadFile = function(url, param, cab, errcbk) {
+export const UploadFile = function (url, param, cab, errcbk) {
   fetch(url, {
     method: 'POST',
     body: param,
   })
-    .then(res => {
+    .then((res) => {
       if (res.headers.get('Content-type').indexOf('application/json') > -1) {
         return res.json();
       }
       return res.text();
     })
-    .then(res => {
-      if (cab) {
-        cab(res);
-      }
-    })
-    .catch(error => {
+    .catch((error) => {
       if (errcbk) {
         errcbk(error);
       }
--- packages/sharewinfo-utils/src/time.js
+++ packages/sharewinfo-utils/src/time.js
@@ -9,21 +9,17 @@ export function getTimeDistance(type) {
     now.setSeconds(0);
     return [moment(now), moment(now.getTime() + (oneDay - 1000))];
   }
-
   if (type === 'week') {
     let day = now.getDay();
     now.setHours(0);
     now.setMinutes(0);
     now.setSeconds(0);
-
     if (day === 0) {
       day = 6;
     } else {
       day -= 1;
     }
-
     const beginTime = now.getTime() - day * oneDay;
-
     return [moment(beginTime), moment(beginTime + (7 * oneDay - 1000))];
   }
 
